dda33b3bde36d10cba0d880b2bebf947d3499071
Diego Pagini <diego.pagini@neoris.com>
2023-10-31 17:23:39 +0100

    form

--- src/app/pages/events-manager/components/add-edit-events/add-edit-events.component.ts
+++ src/app/pages/events-manager/components/add-edit-events/add-edit-events.component.ts
@@ -5,10 +5,16 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './add-edit-events.component.html',
   styleUrls: ['./add-edit-events.component.scss'],
 })
-export class AddEditEventsComponent  implements OnInit {
+export class AddEditEventsComponent implements OnInit {
+  showEnd = false;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {}
+  ngOnInit(): void {}
 
+  addEditEvent(): void {}
+
+  changeShowEnd(): void {
+    this.showEnd = !this.showEnd;
+  }
 }
