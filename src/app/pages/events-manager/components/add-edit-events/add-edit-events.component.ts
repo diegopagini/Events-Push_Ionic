1e1d15d6e16f8af9168dbbcf5651821c12379081
Diego Pagini <diego.pagini@neoris.com>
2023-11-02 13:57:50 +0100

    edit event

--- src/app/pages/events-manager/components/add-edit-events/add-edit-events.component.ts
+++ src/app/pages/events-manager/components/add-edit-events/add-edit-events.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import * as moment from 'moment';
+import { EventDDR } from 'src/app/interfaces/event.ddr';
 
 @Component({
   selector: 'app-add-edit-events',
@@ -6,15 +9,76 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-edit-events.component.scss'],
 })
 export class AddEditEventsComponent implements OnInit {
+  edit = false;
+  event: EventDDR;
+  eventForm: FormGroup;
   showEnd = false;
 
-  constructor() {}
+  constructor(private fb: FormBuilder) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.initEvents();
+  }
 
-  addEditEvent(): void {}
+  addEditEvent(): void {
+    console.log(this.eventForm.value);
+  }
 
   changeShowEnd(): void {
     this.showEnd = !this.showEnd;
+
+    if (!this.showEnd)
+      this.eventForm.patchValue({
+        end: moment().format('YYYY-MM-DDTHH:mm'),
+      });
+    else
+      this.eventForm.patchValue({
+        end: null,
+      });
+  }
+
+  newEvent(): void {
+    this.edit = false;
+    this.showEnd = false;
+    this.event = null as any;
+
+    this.eventForm.patchValue({
+      description: null,
+      end: moment().format('YYYY-MM-DDTHH:mm'),
+      start: moment().format('YYYY-MM-DDTHH:mm'),
+      title: null,
+      type: 'blog',
+      url: null,
+    });
+  }
+
+  private initEvents(): void {
+    if (!this.event) {
+      this.edit = false;
+      this.showEnd = false;
+    } else {
+      this.edit = true;
+      this.showEnd = this.event.end !== null;
+    }
+
+    this.eventForm = this.fb.group({
+      description: [this.event?.description || null, [Validators.required]],
+      end: [this.event?.end || moment().format('YYYY-MM-DDTHH:mm')],
+      start: [
+        this.event?.start || moment().format('YYYY-MM-DDTHH:mm'),
+        [Validators.required],
+      ],
+      title: [this.event?.title || null, [Validators.required]],
+      type: [this.event?.type || 'blog', [Validators.required]],
+      url: [
+        this.event?.url || null,
+        [
+          Validators.required,
+          Validators.pattern(
+            '(https?://)?([\\da-z.-]+)\\.([a-z.]{2,6})[/\\w .-]*/?'
+          ),
+        ],
+      ],
+    });
   }
 }
